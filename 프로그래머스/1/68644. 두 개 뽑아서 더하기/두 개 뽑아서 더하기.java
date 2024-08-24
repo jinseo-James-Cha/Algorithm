90c4274a3525638264eded2e1aeb5229932c9e26
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2024-08-24 22:51:44 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 10.70 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,11 +1,14 @@
 // 1. get all possible cases to pick two numbers in the array
 // for 1's cases, insert them into a array and remove duplicates
 // order ascending and return the answer
+
+// WOW !! instead of using HashSet, I could use TreeSet to have asc order without sorting!
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        HashSet<Integer> set = new HashSet<>();
+        // HashSet<Integer> set = new HashSet<>();
+        Set<Integer> set = new TreeSet<>();
         
         for(int i = 0; i < numbers.length - 1; i++) {
             for(int j = i+1; j < numbers.length; j++ ){
@@ -14,6 +17,6 @@ class Solution {
             }
         }
         
-        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+        return set.stream().mapToInt(Integer::intValue).toArray();
     }
 }
