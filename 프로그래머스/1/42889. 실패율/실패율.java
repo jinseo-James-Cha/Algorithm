8f03087b5cb9df2d02b005efed4dfdc8a6a2f4f1
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2024-08-27 09:44:20 +0900

    [level 1] Title: 실패율, Time: 25.08 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -28,3 +28,59 @@ class Solution {
         return fails.entrySet().stream().sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue())).mapToInt(HashMap.Entry::getKey).toArray();
     }
 }
+
+/* This is another answer I like.
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int N, int[] lastStages) {
+        int nPlayers = lastStages.length;
+        int[] nStagePlayers = new int[N + 2];
+        for (int stage : lastStages) {
+            nStagePlayers[stage] += 1;
+        }
+
+        int remainingPlayers = nPlayers;
+        List<Stage> stages = new ArrayList<>();
+        for (int id = 1 ; id <= N; id++) {
+            double failure = (double) nStagePlayers[id] / remainingPlayers;
+            remainingPlayers -= nStagePlayers[id];
+
+            Stage s = new Stage(id, failure);
+            stages.add(s);
+        }
+        Collections.sort(stages, Collections.reverseOrder());
+
+        int[] answer = new int[N];
+        for (int i = 0; i < N; i++) {
+            answer[i] = stages.get(i).id;
+        }
+        return answer;
+    }
+
+    class Stage implements Comparable<Stage> {
+        public int id;
+        public double failure;
+
+        public Stage(int id_, double failure_) {
+            id = id_;
+            failure = failure_;
+        }
+
+        @Override
+        public int compareTo(Stage o) {
+            if (failure < o.failure ) {
+                return -1;
+            }
+            if (failure > o.failure ) {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
+
+*/
